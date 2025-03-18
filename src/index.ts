1f24317b0369d5f44de7e136488f06af22d3008c
DuongHoaiAn2k1 <dhan29112001@gmail.com>
2025-03-18 13:10:15 +0700

    Setup DB Connection

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express'
+import databaseService from './services/database.services'
 const app = express()
 const port = 4000
+
+databaseService.connect()
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
