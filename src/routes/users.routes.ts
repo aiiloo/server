414885545999f68bc7334b20a0804d9ecbf7ea5c
DuongHoaiAn2k1 <dhan29112001@gmail.com>
2025-03-23 17:21:06 +0700

    Update logut handle

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express'
 import {
   loginController,
+  logoutController,
   oauthController,
   registerController,
   verifyEmailController
 } from '~/controllers/users.controllers'
-import { emailVerifyTokenValidator, loginValidator, registerValidator } from '~/middlewares/users.middlewares'
+import {
+  accessTokenValidator,
+  emailVerifyTokenValidator,
+  loginValidator,
+  refreshTokenValidator,
+  registerValidator
+} from '~/middlewares/users.middlewares'
 import { wrapRequestHandler } from '~/utils/handlers'
 
 const usersRouter = Router()
@@ -18,5 +25,6 @@ usersRouter.post('/login', loginValidator, wrapRequestHandler(loginController))
 usersRouter.get('/oauth/google', wrapRequestHandler(oauthController))
 usersRouter.post('/register', registerValidator, wrapRequestHandler(registerController))
 usersRouter.post('/verify-email', emailVerifyTokenValidator, wrapRequestHandler(verifyEmailController))
+usersRouter.post('/logout', accessTokenValidator, refreshTokenValidator, wrapRequestHandler(logoutController))
 
 export default usersRouter
